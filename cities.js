07a95c5221ed2ac2faa9ce344523feeb8dc106f6
niko1499 <nxgamarra@gmail.com>
2025-10-04 17:24:05 -0500

    Adding world travel section

---
+++ cities.js
@@ -0,0 +1,64 @@
+CITIES = [
+
+    { name: 'Bologna', latitude: 44.4949, longitude: 11.3426, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Chicago', latitude: 41.8832, longitude: -87.6324, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'West Lafayette', latitude: 40.4259, longitude: -86.9081, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Pueblo', latitude: 38.2706, longitude: -104.6101, radius: 3, fillKey: 'city', date: '1995-12' },
+    //
+    { name: 'Bangkok', latitude: 13.7563, longitude: 100.5018, radius: 3, fillKey: 'city', date: '1995-12' },
+
+    { name: 'Barcelona', latitude: 41.3874, longitude: -2.1686, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Vitoria-Gasteiz', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    // below need updating of lat long
+    { name: 'Madrid', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Tenerife', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Skopje', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+
+    { name: 'Shanghai', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Copenhagen', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+
+    { name: 'Innsbruck', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Candelaria', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Barranquilla', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Bogotá', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Medellín', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Santa Marta', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Traverse City', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Philadelphia', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'New York', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Belize City', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Cancún', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'New Orleans', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'St. Louis', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Gary', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Indyanapolis', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Denver', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Sault Ste. Marie', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Mackinac Island', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Billund', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+
+
+    { name: 'Gothenburg', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Tokyo', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Paris', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Paris', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+
+    { name: 'Hamburg', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Worcester', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Boston', latitude: 42.8532, longitude: 2.6732, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'San Fransisco', latitude: 38.2706, longitude: -104.6101, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Seattle', latitude: 38.2706, longitude: -104.6101, radius: 3, fillKey: 'city', date: '1995-12' },
+    { name: 'Vancouver', latitude: 38.2706, longitude: -104.6101, radius: 3, fillKey: 'city', date: '1995-12' },
+
+]
---
+++ countries.js
@@ -0,0 +1,17 @@
+// Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+COUNTRIES = {
+    ITA: { fillKey: 'visited' },
+    USA: { fillKey: 'visited' },
+    THA: { fillKey: 'visited' },
+    FRA: { fillKey: 'visited' },
+    SWE: { fillKey: 'visited' },
+    CHN: { fillKey: 'visited' },
+    COL: { fillKey: 'visited' },
+    ESP: { fillKey: 'visited' },
+    DNK: { fillKey: 'visited' },
+    DEU: { fillKey: 'visited' },
+    JPN: { fillKey: 'visited' },
+    MEX: { fillKey: 'visited' },
+    FRA: { fillKey: 'visited' },
+
+}
