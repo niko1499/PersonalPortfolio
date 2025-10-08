454be72004b819a5fb5aade3f5b1e114ccda0bd3
niko1499 <nxgamarra@gmail.com>
2025-10-07 22:52:52 -0500

    correcting project categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,15 +81,19 @@ const filterItems = document.querySelectorAll("[data-filter-item]");
 const filterFunc = function(selectedValue) {
 
     for (let i = 0; i < filterItems.length; i++) {
+        const dataCat = filterItems[i].dataset.category ? filterItems[i].dataset.category.toLowerCase() : "";
+        const categories = dataCat.split(" ");
 
         if (selectedValue === "all") {
             filterItems[i].classList.add("active");
-        } else if (selectedValue === filterItems[i].dataset.category) {
+        } else if (
+            dataCat === selectedValue ||
+            categories.includes(selectedValue)
+        ) {
             filterItems[i].classList.add("active");
         } else {
             filterItems[i].classList.remove("active");
         }
-
     }
 
 }
